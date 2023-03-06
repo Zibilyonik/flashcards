7ed2afa328e88a649562fae5c9c2f98a6193bda5
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-03-06 22:43:51 +0100

    added basic functionality for checking answers

--- flashcards.go
+++ flashcards.go
@@ -1,10 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Card:")
-	fmt.Println("Potato.")
-	fmt.Println("Definition:")
-	fmt.Println("POTATO")
+	// first part of the project
+	// fmt.Println("Card:")
+	// fmt.Println("Potato.")
+	// fmt.Println("Definition:")
+	// fmt.Println("POTATO")
+	termReader := bufio.NewReader(os.Stdin)
+	defReader := bufio.NewReader(os.Stdin)
+	ansReader := bufio.NewReader(os.Stdin)
+	term, _ := termReader.ReadString('\n')
+	def, _ := defReader.ReadString('\n')
+	ans, _ := ansReader.ReadString('\n')
+	term = strings.TrimSpace(term)
+	def = strings.TrimSpace(def)
+	ans = strings.TrimSpace(ans)
+	if ans == def {
+		fmt.Println("Your answer is right!")
+	} else {
+		fmt.Println("Your answer is wrong...")
+	}
 }
