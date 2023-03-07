1c6666f7b161b4ef5430420393010284bc8723f2
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-03-07 15:39:17 +0100

    added storage for user created cards for the session. Cards can be practiced in the same order they've been added

--- flashcards.go
+++ flashcards.go
@@ -13,18 +13,39 @@ func main() {
 	// fmt.Println("Potato.")
 	// fmt.Println("Definition:")
 	// fmt.Println("POTATO")
-	termReader := bufio.NewReader(os.Stdin)
-	defReader := bufio.NewReader(os.Stdin)
-	ansReader := bufio.NewReader(os.Stdin)
-	term, _ := termReader.ReadString('\n')
-	def, _ := defReader.ReadString('\n')
-	ans, _ := ansReader.ReadString('\n')
-	term = strings.TrimSpace(term)
-	def = strings.TrimSpace(def)
-	ans = strings.TrimSpace(ans)
-	if ans == def {
-		fmt.Println("Your answer is right!")
-	} else {
-		fmt.Println("Your answer is wrong...")
+	var cardCount int
+	fmt.Println("Input the number of cards:")
+	fmt.Scan(&cardCount)
+	terms := make([]string, cardCount)
+	definitions := make([]string, cardCount)
+	for i := 0; i < cardCount; i++ {
+		fmt.Printf("Input the term for card #%d \n", i+1)
+		termReader := bufio.NewReader(os.Stdin)
+		defReader := bufio.NewReader(os.Stdin)
+		term, _ := termReader.ReadString('\n')
+		fmt.Printf("Input the definition for card #%d \n", i+1)
+		def, _ := defReader.ReadString('\n')
+		term = strings.TrimSpace(term)
+		def = strings.TrimSpace(def)
+		terms[i] = term
+		definitions[i] = def
 	}
+	for i := 0; i < cardCount; i++ {
+		fmt.Printf("Print the definition of \"%s\" \n", terms[i])
+		ansReader := bufio.NewReader(os.Stdin)
+		ans, _ := ansReader.ReadString('\n')
+		ans = strings.TrimSpace(ans)
+		if ans == definitions[i] {
+			fmt.Println("Correct!")
+		} else {
+			fmt.Printf("Wrong. The right answer is \"%s\" \n", definitions[i])
+		}
+	}
+	//second part of the project
+	// 	if ans == def {
+	// 		fmt.Println("Your answer is right!")
+	// 	} else {
+	// 		fmt.Println("Your answer is wrong...")
+	// 	}
+	// }
 }
